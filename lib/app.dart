0b96bc1f0826b9f9d5f1d795b0d05ee06fa2eab5
Karen Ghorbanian <k.ghorbanian@yaama.tech>
2021-10-20 19:15:47 +0300

    clean the project from redundant codes and comments

---
+++ lib/app.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Empty Page',
+        ),
+      ),
+    );
+  }
+}
